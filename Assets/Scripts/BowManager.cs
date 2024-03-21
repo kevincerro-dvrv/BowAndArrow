43ba612f9d71fd9ba13e99874fd20313de2af4f4
Kevin Cerro DVRV <kevincerro.dvrv@gmail.com>
2024-03-21 18:48:12 +0100

    Set bow velocity to 0 when restoring position

--- Assets/Scripts/BowManager.cs
+++ Assets/Scripts/BowManager.cs
@@ -4,6 +4,7 @@ public class BowManager : MonoBehaviour
 {
     public static BowManager instance;
 
+    private Rigidbody rb;
     private Vector3 startPosition;
     private Quaternion startRotation;
 
@@ -12,11 +13,18 @@ public class BowManager : MonoBehaviour
     }
 
     void Start() {
+        rb = GetComponent<Rigidbody>();
+
         startPosition = transform.position;
         startRotation = transform.rotation;
     }
 
     public void RestorePosition() {
+        // Restore velocity
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        // Restore position and rotation 
         transform.position = startPosition;
         transform.rotation = startRotation;
     }
